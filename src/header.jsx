2fc86e9415161f1699f5745407a619333bc50b12
shannoj <69207315+shannoj@users.noreply.github.com>
2025-09-24 12:46:14 -0700

    set up basic header

--- src/header.jsx
+++ src/header.jsx
@@ -2,14 +2,14 @@ import React from 'react';
 
 function Header() {
     return (
-        <header className='w-screen flex items-center'>
-            <img src="logo.png" alt="Company Logo" className="logo" />
-            <nav>
-                <ul className='items-center'>
-                    <li><a href="/">Home</a></li>
-                    <li><a href="/about">About</a></li>
-                    <li><a href="/services">Services</a></li>
-                    <li><a href="/contact">Contact</a></li>
+        <header className='w-auto flex justify-between'>
+            <a className='py-2 flex justify-center items-center'>Jamie Shannon</a>
+            <nav className='flex justify-center'>
+                <ul className='py-2 flex justify-center content-center'>
+                    <li className='m-5 content-center'><a href="/">Home</a></li>
+                    <li className='m-5 content-center'><a href="/about">About</a></li>
+                    <li className='m-5 content-center'><a href="/services">Services</a></li>
+                    <li className='m-5 content-center'><a href="/contact">Contact</a></li>
                 </ul>
             </nav>
         </header>
